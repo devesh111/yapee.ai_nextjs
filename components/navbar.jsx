351bb81ec4f9ffe100d3f1490a45ebfd9381926a
devesh111 <mailme.devesh1990@gmail.com>
2026-01-07 22:33:01 +0530

    updated

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
             }`}
         >
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-14 lg:h-16">
+                <div className="flex items-center justify-between h-15 lg:h-18">
                     {/* Logo */}
                     <a href="/#" className="flex items-center gap-2">
                         <span className="text-2xl font-bold gradient-text">
