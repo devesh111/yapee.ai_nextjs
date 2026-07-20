8b13211a7cb8a1b12e9c6fc1e29a01d41d79ea37
devesh111 <mailme.devesh1990@gmail.com>
2026-07-20 20:25:49 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,13 @@
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/trial",
+                destination: "https://buy.stripe.com/00w6oA09O0jx704eV59R600",
+                permanent: true, // 307 redirect
+            },
+        ];
+    },
+};
 
 export default nextConfig;
